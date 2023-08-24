fbae23404005330a3977a413ea853c13b3733351
SKY <88556477+SKY1272@users.noreply.github.com>
2023-08-24 17:49:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+export default {
+  build: {
+    rollupOptions: {
+      external: ['@mui/material'],
+    },
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
